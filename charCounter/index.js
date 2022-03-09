57c968c9c71a303c5c152216e5bdc92821550b56
jp <69607974+letsDoTacos@users.noreply.github.com>
2022-03-08 20:50:07 -0600

    completed char counter

--- charCounter/index.js
+++ charCounter/index.js
@@ -1,2 +1,10 @@
-// alert('taco time!!');
+// create global vars
+const textarea = document.querySelector('textarea');
+const counter = document.querySelector('.counter');
 
+// run on keyup
+function countChar() {
+  const text = textarea.value;
+  const textLength = textarea.value.length;
+  counter.innerText = `${textLength}`;
+ }
