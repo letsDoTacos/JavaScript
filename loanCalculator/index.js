52c05c14b1de80833ffe313ea9a88e75083f9190
jp <69607974+letsDoTacos@users.noreply.github.com>
2022-03-11 15:18:59 -0600

    pushing js with a but to fix later

--- loanCalculator/index.js
+++ loanCalculator/index.js
@@ -1,1 +1,18 @@
-alert("taco time");
+// alert("taco time");
+
+function computeLoan() {
+   const amount = document.querySelector('#amount').value;
+   const interest_rate = document.querySelector('#intrest_rate').arivalueaValueMax;
+   const months = document.querySelector('#months').value;
+
+   const interest = (amount * (interest_rate * 0.01)) / months;
+
+  //  calculate payment 
+  let payment = ((amount / months) + interest).toFixed(2);
+
+  //  add a comma after ever 3rd diget
+  payment = payment.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ","); 
+
+  //  innerHTML fto display info
+  document.querySelector('#payment').innerHTML = `Monthly Payment = ${payment}`
+}
