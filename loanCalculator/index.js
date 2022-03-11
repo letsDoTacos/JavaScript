c8ed09eea456f5924a39aee6405fa0a5409fc5e2
jp <69607974+letsDoTacos@users.noreply.github.com>
2022-03-11 16:45:07 -0600

    completed loan calculator

--- loanCalculator/index.js
+++ loanCalculator/index.js
@@ -2,12 +2,12 @@
 
 function computeLoan() {
    const amount = document.querySelector('#amount').value;
-   const interest_rate = document.querySelector('#intrest_rate').arivalueaValueMax;
+   const interest_rate = document.querySelector('#intrest_rate').value;
    const months = document.querySelector('#months').value;
 
    const interest = (amount * (interest_rate * 0.01)) / months;
 
-  //  calculate payment 
+  //  calculate payment w/ to 2 decimal points
   let payment = ((amount / months) + interest).toFixed(2);
 
   //  add a comma after ever 3rd diget
