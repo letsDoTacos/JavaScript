52ca0943d0c6848a815da46ba141d5a8740f729c
jp <69607974+letsDoTacos@users.noreply.github.com>
2022-03-14 11:15:26 -0500

    completed mouse position project

--- mousePosition/index.js
+++ mousePosition/index.js
@@ -1,2 +1,11 @@
 // alert('taco time!!!');
 
+// 
+let output = document.getElementById('output');
+
+// 
+window.addEventListener("mousemove", (e) => {
+  let xPos = e.clientX;
+  let yPos = e.clientY;
+  output.innerHTML = `<div><span>X: </span>${xPos}px</div><div><span>Y: </span>${yPos}px</div>`;
+});
