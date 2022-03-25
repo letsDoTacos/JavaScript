68da522140d1d5d77acfe2525e7d97e16f5890e8
jp <69607974+letsDoTacos@users.noreply.github.com>
2022-03-24 21:39:02 -0500

    date and time are functional but colons are not flashing

--- dateTime/index.js
+++ dateTime/index.js
@@ -1,19 +1,15 @@
 // alert('taco time!!');
 
-const hours = document.querySelector('hours');
+const hours = document.querySelector('.hours');
 // console.log(hours);
-const minuets = document.querySelector('minuets');
-// console.log(minuets);
-const seconds = document.querySelector('seconds');
-// console.log(seconds);
+const minuets = document.querySelector('.minuets');
 
-const month = document.querySelector('month');
-// console.log(month);
-const day = document.querySelector('day');
-// console.log(month);
-const year = document.querySelector('years');
+const seconds = document.querySelector('.seconds');
+
+const month = document.querySelector('.month');
+const day = document.querySelector('.day');
+const year = document.querySelector('.year');
 // console.log(year);
-
 
 function setDate() {
 
@@ -23,15 +19,20 @@ function setDate() {
   // now you can uwe the info; month, day and year from the device 
 
   const mm = now.getMonth();
+  // console.log(mm);
   const dd = now.getDate();
+  // console.log(dd);
   const yyyy = now.getFullYear();
   // console.log(yyyy);
 
-  // info for sec, mins and hrs
-  const secs = now.getSeconds();
-  const mins = now.getMinutes();
+  // info for hrs, mins secs
   const hrs = now.getHours();
   // console.log(hrs);
+  const mins = now.getMinutes();
+  // console.log(mins);
+  const secs = now.getSeconds();
+  // console.log(secs);
+ 
 
   // months
   const monthName = [
@@ -39,28 +40,31 @@ function setDate() {
     'May', 'June', 'July', 'August', 'September',
     'October', 'November', 'December'];
   
-
   // Add a zero when the time is below 10
   if (hrs < 10) {
     hours.innerHTML = '0' + hrs;
   } else {
     hours.innerHTML = hrs;
+    // console.log(hrs);
   }
 
-  if(secs < 10) {
+  if (mins < 10) {
+    minuets.innerHTML = '0' + mins;
+  } else {
+    minuets.innerHTML = mins;
+    // console.log(mins);
+  }
+
+  if (secs < 10) {
     seconds.innerHTML = '0' + secs;
   } else {
     seconds.innerHTML = secs;
   }
-  if(mins < 10) {
-    minuets.innerHTML = '0' + secs;
-  } else {
-    minuets.innerHTML = secs;
-  }
+
   // date
   month.innerHTML = monthName[mm];
   day.innerHTML = dd;
   year.innerHTML = yyyy;
-}
+};
 
 setInterval(setDate, 1000);
