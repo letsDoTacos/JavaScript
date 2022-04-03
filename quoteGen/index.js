cba264e5bd373f3ba8f70da378576ba9c59db676
jp <69607974+letsDoTacos@users.noreply.github.com>
2022-04-03 17:04:55 -0500

    rando quote gen

--- quoteGen/index.js
+++ quoteGen/index.js
@@ -1,1 +1,20 @@
-alert('Peanut Butter Jelly Time!!!');
+// alert('Peanut Butter Jelly Time!!!');
+
+let quote = document.getElementById("quote");
+let author = document.getElementById("author");
+let btn = document.getElementById("btn");
+
+const url = "https://api.quotable.io/random";
+
+let getQuote = () => {
+  fetch(url)
+    .then((data) => data.json())
+    .then((item) => {
+      quote.innerText = item.content;
+      author.innerHTML = item.author;
+    });
+};
+
+window.addEventListener("load", getQuote);
+
+btn.addEventListener("click", getQuote);
